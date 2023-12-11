1df9a12a09aa8caa67d1d540ec75486d21bf3185
Jark B. <31354336+jark-b90@users.noreply.github.com>
2023-12-11 19:58:15 +0200

    Added basic files, credits, publish.bat and others

---
+++ .publish.bat
@@ -0,0 +1,3 @@
+cd src\Opswat.Challenge\Opswat.Challenge
+dotnet publish -c release /p:DebugType=None /p:DebugSymbols=false
+@pause
---
+++ src/Opswat.Challenge/Opswat.Challenge/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace Opswat.Challenge
+{
+    internal class Program
+    {
+        private const string BaseApiUri = "https://api.metadefender.com/v4";
+        private const string GetByDataIdUri = BaseApiUri + "/file/{0}";
+        private const string GetByHashUri = BaseApiUri + "/hash/{0}";
+        private const string PostFileUri = BaseApiUri + "/file";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
